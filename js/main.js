865c5dfcdbb4bdf3741c7b8ecd69de6ab1808a22
aliens-designer <karim2002amin@gmail.com>
2023-07-19 22:38:41 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let subBtn = document.getElementById('subBtn')
+let userMail = document.getElementById('userMail')
+let emailValidation = document.getElementById('emailValidation')
+let backToHomeBtn = document.getElementById('backToHomeBtn')
+let successSection = document.getElementById('successSection')
+let mainForm = document.getElementById('mainForm')
+let userMailSuccess = document.getElementById('userMailSuccess')
+
+let regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+function subToNewsLetter() {
+    if (regex.test(userMail.value)) {
+        userMailSuccess.innerHTML = userMail.value
+        userMail.value=``
+        emailValidation.innerHTML=``
+        successSub()
+        // console.log(localStorage.getItem("mail"))
+    }else{
+        emailValidation.innerHTML="Please Enter Valid Mail"
+    }
+}
+function successSub(){
+    successSection.style.display="flex";
+    mainForm.style.display="none"
+}
+function backToHome(){
+    successSection.style.display="none";
+    mainForm.style.display="flex"
+}
